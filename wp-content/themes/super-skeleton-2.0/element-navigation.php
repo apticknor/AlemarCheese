74051815098a07137c2b50f0fb3de6737b649391
Anthony Ticknor <tony.ticknor@gmail.com>
2013-09-29 11:12:20 -0500

    modifying full width template to use css columbs for better layout at a larger size

--- wp-content/themes/super-skeleton-2.0/element-navigation.php
+++ wp-content/themes/super-skeleton-2.0/element-navigation.php
@@ -1,10 +1,10 @@
 		<!-- Menu -->
-		<div class="sixteen columns" id="menu"> 
-			
+		<div class="sixteen columns" id="menu">
+
 			<?php if(get_option_tree('header_social') == 'No') { $header_social = 'sixteen omega'; } ?>
-			
+
 			<div class="twelve columns alpha navigation <?php echo $header_social; ?>">
-				
+
 				<!-- DEFAULT NAVIGATION -->
 				<?php wp_nav_menu( array(
 					 'container' =>false,
@@ -19,14 +19,14 @@
 					 'walker' => new description_walker())
 				 ); ?>
 				 <!-- /DEFAULT NAVIGATION -->
-				 
-				 
+
+
 				 <!-- RESPONSIVE NAVIGATION FLIP -->
 				<form id="responsive-nav" action="" method="post">
 				<select class="chzn-select">
 				<option value="">Navigation</option>
-				<?php 
-				
+				<?php
+
 				$menu = wp_nav_menu(array('theme_location' => 'topbar_small', 'echo' => false));
 				   if (preg_match_all('#(<a [^<]+</a>)#',$menu,$matches)) {
 				      $hrefpat = '/(href *= *([\"\']?)([^\"\' ]+)\2)/';
@@ -40,21 +40,21 @@
 				         }
 				         echo "<option value=\"$href\">$name</option>";
 				      }
-				   }				
-				
+				   }
+
 				?>
 				</select>
 				</form>
 				<!-- /END RESPONSIVE NAV -->
-							 
-			</div>	
-			
-			<?php if (get_option_tree('header_social') == 'No') { } else { ?>	
+
+			</div>
+
+			<?php if (get_option_tree('header_social') == 'No') { } else { ?>
 			<div class="four columns omega" id="tagline">
 				<?php get_template_part( 'element', 'getsocial' ); ?>
-			</div>	
+			</div>
 			<?php } ?>
-			
+
 			<hr class="remove-top"/>
-		</div>		 
+		</div>
 		<!-- /End Menu -->
