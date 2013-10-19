25f7b7b420506f7e22be46d1037cb8b5a85d2963
Anthony Ticknor <tony.ticknor@gmail.com>
2013-10-18 21:35:32 -0500

    functional buttons for paypal checkout from the order screen

---
+++ wp-content/themes/super-skeleton-2.0/template-order-now.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Template Name: Order Now
+*/
+
+get_header();
+?>
+
+
+<!-- ============================================== -->
+
+<!-- Bent River -->
+<h2>Bent River</h2>
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="cmd" value="_s-xclick">
+    <input type="hidden" name="hosted_button_id" value="3MB7Z7T5JNMUG">
+    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+<!-- Good Thunder -->
+<h2>Good Thunder</h2>
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="cmd" value="_s-xclick">
+    <input type="hidden" name="hosted_button_id" value="Y87SM2UPQRZVA">
+    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+<!-- Fromage Blanc -->
+<h2>Fromage Blanc</h2>
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="cmd" value="_s-xclick">
+    <input type="hidden" name="hosted_button_id" value="CHZHNBX2JCJNA">
+    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+<!-- ============================================== -->
+
+<?php get_footer(); ?>
