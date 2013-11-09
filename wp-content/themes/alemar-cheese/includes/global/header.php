07ef8da0561edf7045ea80d93aa9b5355ca48c13
Anthony Ticknor <tony.ticknor@gmail.com>
2013-11-09 14:46:57 -0600

    setting up working nav in theme

--- wp-content/themes/alemar-cheese/includes/global/header.php
+++ wp-content/themes/alemar-cheese/includes/global/header.php
@@ -1,12 +1,27 @@
                 <div class="wrapper">
-                    <div role="banner">
+                    <div class="masthead" role="banner">
                         <h1>
                             <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
                         </h1>
+                        <div role="navigation">
+                            <ul>
+                                <li>
+                                    <a href="<?php echo get_page_link(205); ?>">Products</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo get_page_link(19); ?>">Blog</a>
+                                </li>
+                                <li>
+                                    <a href="<?php $category_id = get_cat_ID('Media'); echo get_category_link($category_id); ?>">Media</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo get_page_link(6); ?>">About Us</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo get_page_link(21); ?>">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
 
-                    <div role="navigation" class="isHidden">
-                        <a href="<?php echo get_page_link(1); ?>">Some Page</a>
-                    </div>
                 </div> <!-- // END wrapper -->
-
