f88c3ab6b9b6dceead158e3566ef44b555a65e96
Anthony Ticknor <tony.ticknor@gmail.com>
2013-11-10 14:16:22 -0600

    adding base files for product archive and detail

---
+++ wp-content/themes/alemar-cheese/archive-ac_product.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Archive
+URL: http://alemarcheese.com/products/
+*/
+?>
+<?php Utilities::get_template_parts( array( 'includes/global/html-header', 'includes/global/header' ) ); ?>
+
+                <div class="wrapper">
+                    <div class="main" role="main">
+                        <h1 class="isHidden">Products</h1>
+                        <div class="grid">
+                            <div class="grid-col grid-col_main">
+
+Product Listing Goes Here
+
+                            </div> <!-- // END grid-col_main -->
+                            <div class="grid-col grid-col_sub">
+
+Product Sidebar Goes Here
+
+                            </div> <!-- // END grid-col_sub -->
+                        </div> <!-- // END grid -->
+                    </div> <!-- // END main -->
+                </div> <!-- // END wrapper -->
+
+<?php Utilities::get_template_parts( array( 'includes/global/footer','includes/global/html-footer' ) ); ?>
---
+++ wp-content/themes/alemar-cheese/single-ac_product.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Page Default Template
+URL: http://alemarcheese.com/YYYY/MM/POST_NAME/
+*/
+?>
+<?php Utilities::get_template_parts( array( 'includes/global/html-header', 'includes/global/header' ) ); ?>
+
+                <div class="wrapper">
+                    <div class="main" role="main">
+
+
+
+
+
+
+
+
+Product Detail Page Goes HEre
+
+
+
+
+
+
+
+                    </div> <!-- // END main -->
+                </div> <!-- // END wrapper -->
+
+<?php Utilities::get_template_parts( array( 'includes/global/footer','includes/global/html-footer' ) ); ?>
