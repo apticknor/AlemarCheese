ca073eb9458b21425d6902b77aff5b0eab5d905d
Anthony Ticknor <tony.ticknor@gmail.com>
2013-11-12 23:21:50 -0600

    adding styled pagination

--- wp-content/themes/alemar-cheese/includes/snippets/pagination.php
+++ wp-content/themes/alemar-cheese/includes/snippets/pagination.php
@@ -1,4 +1,4 @@
 <ol class="pagination">
-    <li><?php next_posts_link( 'Older posts' ); ?></li>
-    <li><?php previous_posts_link( 'Newer posts' ); ?></li>
+    <li class="pagination-older"><?php next_posts_link( 'Older posts' ); ?></li>
+    <li class="pagination-newer"><?php previous_posts_link( 'Newer posts' ); ?></li>
 </ol>
