1da987bb82d4a896cda9467a390f0a33c318a96e
Anthony Ticknor <tony.ticknor@gmail.com>
2013-11-16 18:21:15 -0600

    adding archive plugin

--- wp-content/themes/alemar-cheese/includes/widgets/archives.php
+++ wp-content/themes/alemar-cheese/includes/widgets/archives.php
@@ -3,6 +3,17 @@
         <h3 class="hdg hdg_sm mix-hdg_caps">Archives</h3>
     </div>
     <div class="widget-bd">
-
+        <ol>
+<?php wp_get_archives(array(
+    'type'            => 'yearly',
+    'limit'           => '',
+    'format'          => 'html',
+    'before'          => '',
+    'after'           => '',
+    'show_post_count' => true,
+    'echo'            => 1,
+    'order'           => 'DESC'
+)); ?>
+        </ol>
     </div>
 </div>
