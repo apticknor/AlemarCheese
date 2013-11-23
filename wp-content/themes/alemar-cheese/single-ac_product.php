c4f677c47f27c33c2067b900f3ebd0a75bc40d45
Anthony Ticknor <tony.ticknor@gmail.com>
2013-11-23 15:57:49 -0600

    changing blocks object to use inline block instead of float

--- wp-content/themes/alemar-cheese/single-ac_product.php
+++ wp-content/themes/alemar-cheese/single-ac_product.php
@@ -3,19 +3,32 @@
                 <div class="main" role="main">
                     <div class="wrapper">
 
+                        <div class="grid">
+                            <div class="grid-col grid-col_main">
 
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+                                <div class="flag">
+<?php if (has_post_thumbnail($post->ID)) { ?>
+                                    <div class="flag-media">
+                                        <?php the_post_thumbnail('medium'); ?>
+                                    </div>
+<?php } ?>
+                                    <div class="flag-bd">
+                                        <h2 class="hdg hdg_lg"><?php the_title(); ?></h2>
+                                        <div class="userContent">
+                                            <?php the_content(); ?>
+                                        </div>
+                                    </div>
+                                </div>
+<?php endwhile; ?>
 
+                            </div> <!-- // END grid-col_main -->
+                            <div class="grid-col grid-col_sub">
 
+<?php Utilities::get_template_parts(array('includes/components/sidebar')); ?>
 
-
-
-
-Product Detail Page Goes HEre
-
-
-
-
-
+                            </div> <!-- // END grid-col_sub -->
+                        </div> <!-- // END grid -->
 
 
                     </div> <!-- // END wrapper -->
