96da2caea392ebfb54f2db9223c30b65ed58ebf6
Anthony Ticknor <tony.ticknor@gmail.com>
2013-11-23 12:53:52 -0600

    product listening design iteration

--- wp-content/themes/alemar-cheese/includes/components/listing-product.php
+++ wp-content/themes/alemar-cheese/includes/components/listing-product.php
@@ -2,15 +2,23 @@
 <ol class="blocks">
 <?php while (have_posts()) : the_post(); ?>
     <li>
-        <a href="<?php the_permalink(); ?>">
-        <?php if (has_post_thumbnail($post->ID)) { ?>
-            <?php the_post_thumbnail('medium'); ?>
-        <?php } else { ?>
-
-        <?php } ?>
-        </a>
-    </li> <!-- // END blocks -->
+        <div class="feature">
+            <div class="feature-media">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('medium'); ?>
+                </a>
+            </div>
+            <div class="feature-hd">
+                <a href="<?php the_permalink(); ?>">
+                    <h2 class="hdg hdg_md"><?php the_title(); ?></h2>
+                </a>
+            </div>
+            <div class="feature-bd">
+                <p><?php the_field('quick_description'); ?></p>
+            </div>
+        </div><!-- // END feature -->
+    </li> <!-- // END blocks-item -->
 <?php endwhile; ?>
-</ol>
+</ol> <!-- // END blocks -->
 <?php Utilities::get_template_parts(array('includes/snippets/pagination')); ?>
 <?php endif; ?>
