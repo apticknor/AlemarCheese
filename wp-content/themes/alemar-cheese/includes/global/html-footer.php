8cf4bb90990cce9d6e62076c1e0f9385783f2416
Anthony Ticknor <tony.ticknor@gmail.com>
2013-11-30 11:47:06 -0600

    close #41 - adding google analytics tracking code along with a field to configure the ID

--- wp-content/themes/alemar-cheese/includes/global/html-footer.php
+++ wp-content/themes/alemar-cheese/includes/global/html-footer.php
@@ -1,4 +1,15 @@
 
 <?php wp_footer(); ?>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("<?php the_field('google_analytics_id', 'options'); ?>");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
+
     </body>
 </html>
