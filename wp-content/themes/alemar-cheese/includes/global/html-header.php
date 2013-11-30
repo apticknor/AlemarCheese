07efe6f4957e7db48984f82121b6fcb84161b0dd
Anthony Ticknor <tony.ticknor@gmail.com>
2013-11-30 11:42:40 -0600

    adding apple touch icons. close #40

--- wp-content/themes/alemar-cheese/includes/global/html-header.php
+++ wp-content/themes/alemar-cheese/includes/global/html-header.php
@@ -2,9 +2,19 @@
 <html class="no-js" lang="en">
     <head>
         <title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
+        
+        <!-- META -->
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- ICONS -->
+        <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72-precomposed.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114-precomposed.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144x144-precomposed.png" />
+        
+        <!-- WP GENERATED -->
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
