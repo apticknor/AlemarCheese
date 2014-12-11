7a2c132a3f4093756f3827f2c2eb6e723f39fb14
Anthony Ticknor <tony.ticknor@gmail.com>
2014-12-10 22:09:43 -0600

    Adding support for repeater style buttons

--- wp-content/themes/alemar-cheese/includes/snippets/paypal-button.php
+++ wp-content/themes/alemar-cheese/includes/snippets/paypal-button.php
@@ -1,5 +1,11 @@
-<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-    <input type="hidden" name="cmd" value="_s-xclick">
-    <input type="hidden" name="hosted_button_id" value="<?php the_field('paypal_add_to_cart_id'); ?>">
-    <button type="submit" name="submit" class="btn btn_buy">Buy Now</button>
-</form>
+<?php if( get_field('paypal_buttons') ): ?>
+    <?php while(has_sub_field('paypal_buttons')) { ?>
+        <div class="buttonGroup">
+            <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="<?php the_sub_field('paypal_button_id'); ?>">
+                <button type="submit" name="submit" class="btn btn_buy"><?php the_sub_field('paypal_button_text'); ?></button>
+            </form>
+        </div><br/>
+    <?php } ?>
+<?php endif; ?>
